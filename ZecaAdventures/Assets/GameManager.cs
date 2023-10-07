3e4180b008c09d295b36b88a128fa8f35694ede2
Bárbara Sakane <sakanebarbara@gmail.com>
2023-10-07 13:50:37 -0300

    gameOverMenu0

--- ZecaAdventures/Assets/GameManager.cs
+++ ZecaAdventures/Assets/GameManager.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,7 +8,32 @@ public class GameManager : MonoBehaviour
 {
   bool gameIsOver = false;
   public float menuDelay = 1f;
+  public float transitionDelay = 3f;
 
+  public GameObject _startingSceneTransition;
+  public GameObject _endingSceneTransition;
+
+  private void Start()
+  {
+    _startingSceneTransition.SetActive(true);
+    Invoke("DisableStartingSceneTransition", transitionDelay);
+    Debug.Log("Start");
+  }
+  private void DisableStartingSceneTransition()
+  {
+    _startingSceneTransition.SetActive(false);
+  }
+
+  private void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.Space))
+    {
+      Debug.Log("Update");
+      _endingSceneTransition.SetActive(true);
+      Invoke("Menu", menuDelay);
+      
+    }
+  }
   public void GameOver()
   {
     if(gameIsOver==false)
@@ -17,6 +45,7 @@ public class GameManager : MonoBehaviour
   }
   void Menu()
   {
+    _endingSceneTransition.SetActive(true);
     SceneManager.LoadScene("GameOverScreen");
   }
 }
