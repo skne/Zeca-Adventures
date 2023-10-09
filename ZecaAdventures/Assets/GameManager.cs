46045db55b08e651bbbce01d47c5ca65dd4bbac1
Bárbara Sakane <sakanebarbara@gmail.com>
2023-10-09 08:23:44 -0300

    timer

--- ZecaAdventures/Assets/GameManager.cs
+++ ZecaAdventures/Assets/GameManager.cs
@@ -8,32 +8,8 @@ public class GameManager : MonoBehaviour
 {
   bool gameIsOver = false;
   public float menuDelay = 1f;
-  public float transitionDelay = 3f;
 
-  public GameObject _startingSceneTransition;
-  public GameObject _endingSceneTransition;
-
-  private void Start()
-  {
-    _startingSceneTransition.SetActive(true);
-    Invoke("DisableStartingSceneTransition", transitionDelay);
-    Debug.Log("Start");
-  }
-  private void DisableStartingSceneTransition()
-  {
-    _startingSceneTransition.SetActive(false);
-  }
-
-  private void Update()
-  {
-    if (Input.GetKeyDown(KeyCode.Space))
-    {
-      Debug.Log("Update");
-      _endingSceneTransition.SetActive(true);
-      Invoke("Menu", menuDelay);
-      
-    }
-  }
+  
   public void GameOver()
   {
     if(gameIsOver==false)
